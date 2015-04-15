341d0ffc7d7890a59afc3de0887eb38ef73a1f50
Jan Tattermusch <jtattermusch@google.com>
2015-04-15 16:53:19 -0700

    Make package installable by native projects as well

--- contrib/coapp/buildall.bat
+++ contrib/coapp/buildall.bat
@@ -31,11 +31,11 @@ REM call :build x64 Release v100 || goto :eof
 REM call :build x64 Debug v100 || goto :eof
 REM endlocal
 
-REM setlocal
-REM call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-REM call :build Win32 Release v100 || goto :eof
-REM call :build Win32 Debug v100 || goto :eof
-REM endlocal
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v100 || goto :eof
+call :build Win32 Debug v100 || goto :eof
+endlocal
 
 goto :eof
 
